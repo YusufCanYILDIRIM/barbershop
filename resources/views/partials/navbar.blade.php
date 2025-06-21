47643b56ca7f14f75e0322201785854ddc045242
Yusuf Can YILDIRIM <yusufyildirimcan@outlook.com>
2025-06-21 15:43:27 +0300

    Enhance booking, contact, navbar, price list, and services sections with improved forms and layout

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,19 +1,30 @@
+  <nav id="sidebarMenu" class="col-md-4 col-lg-3 d-md-block sidebar collapse p-0">
 
-<section class="hero-section d-flex justify-content-center align-items-center" id="section_1">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-12">
-                <h1 class="text-white mb-lg-3 mb-4"><strong>Barber <em>Shop</em></strong></h1>
-                <p class="text-black">Get the most professional haircut for you</p>
-                <br>
-                <a class="btn custom-btn custom-border-btn custom-btn-bg-white smoothscroll me-2 mb-2" href="#section_2">About Us</a>
-                <a class="btn custom-btn smoothscroll mb-2" href="#section_3">What we do</a>
-            </div>
-        </div>
-    </div>
-    <div class="custom-block d-lg-flex flex-column justify-content-center align-items-center">
-        <img src="{{ asset('images/vintage-chair-barbershop.jpg') }}" class="custom-block-image img-fluid" alt="">
-        <h4><strong class="text-white">Hurry Up! Get good haircut.</strong></h4>
-        <a href="#booking-section" class="smoothscroll btn custom-btn custom-btn-italic mt-3">Book a seat</a>
-    </div>
-</section>
+                    <div class="position-sticky sidebar-sticky d-flex flex-column justify-content-center align-items-center">
+                        <a class="navbar-brand" href="index.html">
+                            <img src="images/templatemo-barber-logo.png" class="logo-image img-fluid" align="">
+                        </a>
+
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_1">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_2">Our Story</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_3">Services</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_4">Price List</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_5">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
